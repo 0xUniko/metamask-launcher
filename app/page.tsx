129f57235a46b97cace36d61675202381f7be6fd
0xuniko <946153376@qq.com>
2023-05-18 15:50:29 +0800

    connect button

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,17 @@ export default function Home() {
   const [metaMask, _hooks] = initializeConnector<MetaMask>(
     (actions) => new MetaMask({ actions })
   );
-  useEffect(() => {
-    metaMask.activate();
-    // void metaMask.connectEagerly().catch(() => {
-    //   console.debug("Failed to connect eagerly to metamask");
-    // });
-  }, []);
+  // useEffect(() => {
+  //   metaMask.activate();
+  // void metaMask.connectEagerly().catch(() => {
+  //   console.debug("Failed to connect eagerly to metamask");
+  // });
+  // }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div onClick={() => metaMask.activate()}>
+        <button>Connect</button>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
