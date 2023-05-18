e9e5b7eeea1997738bc56ef8352e2b95b440b645
0xuniko <946153376@qq.com>
2023-05-18 16:00:49 +0800

    id=connect

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   // }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div onClick={() => metaMask.activate()}>
+      <div id="connect" onClick={() => metaMask.activate()}>
         <button>Connect</button>
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
